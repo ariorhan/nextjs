5c9299112979ee07902d3e0f9fd0053e9a46ef21
Orhan <orhnari@gmail.com>
2023-07-29 21:47:08 +0100

    UI

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,10 +14,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      <Navbar/>  
-      deneme  
-      {children}
-      <Footer/>
+        <div className="container">
+          <Navbar/>  
+          {children}
+          <Footer/>
+        </div>
       </body>
     </html>
   )
